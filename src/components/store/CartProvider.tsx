2aa3c921c5a1de687a136aae71c58fe9b81989ad
Jon-Morgan <jonmorgan.cobb@gmail.com>
2023-03-11 15:14:36 -0600

    cart provider typing added, enum added, fix ts error

--- src/components/store/CartProvider.tsx
+++ src/components/store/CartProvider.tsx
@@ -1,21 +1,71 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cartContext";
+// NOTE: Went with useReducer here instead of usestate because the logic to manage state will be complex
 
-const CartProvider = (props: any) => {
-  const addItemToCart = (item: any) => {};
+enum ACTION_TYPES {
+  ADD = "ADD_ITEM_TO_CART",
+  REMOVE = "REMOVE_ITEM_FROM_CART",
+}
 
-  const removeItemFromCart = (id: string) => {};
+interface ItemsType {
+  items: [];
+}
+
+interface DefaultCartStateTypes {
+  items: ItemsType[];
+  totalAmount: number;
+}
+
+interface CartProviderPropTypes {
+  children: React.ReactNode;
+}
+
+// NOTE: Default store state for cart
+const defaultCartState: DefaultCartStateTypes = {
+  items: [],
+  totalAmount: 0,
+};
+
+// NOTE: Fix this ts error
+const cartReducer = (state: any, action: any) => {
+  if (action.type === ACTION_TYPES.ADD) {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
+// NOTE: This is where we manage our cart data
+const CartProvider = ({ children }: CartProviderPropTypes) => {
+  // NOTE: cartState is out initial state, dispatchCartAction is our function to update what kind of event is happening
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCart = (item: any) => {
+    // NOTE: Same as item: item
+    dispatchCartAction({ type: ACTION_TYPES.ADD, item });
+  };
+
+  const removeItemFromCart = (id: string) => {
+    // NOTE: Same as id: id
+    dispatchCartAction({ type: ACTION_TYPES.REMOVE, id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCart,
     removeItem: removeItemFromCart,
   };
   return (
-    <CartContext.Provider value={cartContext}>
-      {props.children}
-    </CartContext.Provider>
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
 };
 
